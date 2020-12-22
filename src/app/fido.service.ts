b5443b423e3bb3f5b5197309dac29d5394a83ab2
Maddie Hawkins <maddie.hawkins@credera.com>
2020-12-22 15:36:24 -0600

    Moved data from list to service and setup connection

--- src/app/fido.service.ts
+++ src/app/fido.service.ts
@@ -1,12 +1,26 @@
+import { Subject } from 'rxjs';
 
 export class FidoService {
 
-  private fidos = [
-    { name: 'Lady', age: 26, profession: 'heiress', likes: ['pearl necklaces', 'dry martinis'], dislikes: ['dirt'], pic: 'https://images.dog.ceo/breeds/papillon/n02086910_7280.jpg'},
-    { name: 'Tramp', age: 28, profession: 'scoundrel', likes: ['leftover pizza', 'bridges','heiresses'], dislikes: ['the patriarchy'], pic: 'https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg'},
+  fidos = [
+    { name: 'Lady', age: 26, profession: 'heiress', hometown: 'London, England', likes: 'pearl necklaces and dry martinis with 3 olives', dislikes: 'dirt', pic: 'https://images.dog.ceo/breeds/papillon/n02086910_7280.jpg'},
+    { name: 'Tramp', age: 28, profession: 'scoundrel', hometown: 'Chicago, USA', likes: 'dumpster diving, leftover pizza, and heiresses', dislikes: 'suits and ties', pic: 'https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg'},
+    { name: 'Biff Jr', age: 23, profession: 'jock', hometown: 'Beaver, OH', likes: 'himself, winning, money, hot chicks', dislikes: 'school, rooms without mirrors', pic: 'https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3.jpg'},
+
   ];
+
+  fidosChanged = new Subject<void>();
 
   constructor() {}
 
+  getFidos(){
+    console.log(this.fidos);
+    return this.fidos.slice();
+  }
 
+  addFido(name, age, profession, hometown, likes, dislikes, pic){
+    const newFido = { name: name, age: age, profession: profession, hometown: hometown, likes: likes, dislikes: dislikes, pic: pic};
+    this.fidos.push(newFido);
+    this.fidosChanged.next();
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,20 +7,17 @@ import { FidoService } from '../fido.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  fidos = [
-    { name: 'Lady', age: 26, profession: 'heiress', hometown: 'London, England', likes: 'pearl necklaces and dry martinis with 3 olives', dislikes: 'dirt', pic: 'https://images.dog.ceo/breeds/papillon/n02086910_7280.jpg'},
-    { name: 'Tramp', age: 28, profession: 'scoundrel', hometown: 'Chicago, USA', likes: 'dumpster diving, leftover pizza, and heiresses', dislikes: 'suits and ties', pic: 'https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg'},
-    { name: 'Biff Jr', age: 23, profession: 'jock', hometown: 'Beaver, OH', likes: 'himself, winning, money, hot chicks', dislikes: 'school, rooms without mirrors', pic: 'https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3.jpg'},
-
-  ];
+  fidos = [];
 
   fidoService: FidoService;
+  subscription;
 
   constructor(fidoService: FidoService) {
     this.fidoService = fidoService;
   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.fidos = this.fidoService.getFidos();
   }
 
 }
