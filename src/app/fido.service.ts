cab7b3063673b5c8a1d281223e252797d3bdd3b8
Maddie Hawkins <maddie.hawkins@credera.com>
2020-12-22 13:39:27 -0600

    Added fido.service and populated some data

---
+++ src/app/fido.service.ts
@@ -0,0 +1,12 @@
+
+export class FidoService {
+
+  private fidos = [
+    { name: 'Lady', age: 26, profession: 'heiress', likes: ['pearl necklaces', 'dry martinis'], dislikes: ['dirt']},
+    { name: 'Tramp', age: 28, profession: 'scoundrel', likes: ['leftover pizza', 'bridges','heiresses'], dislikes: ['the patriarchy']},
+  ];
+
+  constructor() {}
+
+
+}
